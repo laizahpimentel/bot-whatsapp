f01e56171e1ce8801e500a4f6a64e70e5adbc00f
Laíza  Pimentel <laizahpimentel@gmail.com>
2024-05-07 00:49:19 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const { Client } = require('whatsapp-web.js');
+const qrcode = require('qrcode-terminal');
+
+const client = new Client();
+
+client.on('ready', () => {
+    console.log('Client is ready!');
+});
+
+client.on('qr', qr => {
+    qrcode.generate(qr, {small: true});
+});
+
+client.initialize();
